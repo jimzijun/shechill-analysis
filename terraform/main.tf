2dc3ebce812c96c8f4169acb812a3b73fdb09c56
Zijun yi <jimzijun@gmail.com>
2025-08-31 15:18:45 -0700

    Release/v1.2.1 (#12)
    * fix: deployment pipeline
    
    * fix: deployment pipeline issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,7 +53,7 @@ resource "null_resource" "docker_build_and_deploy" {
   # Note: Docker image is now built by GitHub Actions workflow
   
   # Note: Docker image is now transferred by GitHub Actions workflow
-  # The image archive should already be available at ~/shechill-analysis.tar.gz on the NAS
+  # The image archive should already be available at /tmp/docker-uploads/shechill-analysis.tar.gz on the NAS
   
   # Deploy container on NAS
   provisioner "local-exec" {
@@ -68,7 +68,7 @@ resource "null_resource" "docker_build_and_deploy" {
         # Ensure directories exist and are writable by container
         ls -la ${var.persistent_data_path}
         
-        cd ~
+        cd /tmp/docker-uploads
         /usr/local/bin/docker load < shechill-analysis.tar.gz
         /usr/local/bin/docker stop ${var.container_name} || true
         /usr/local/bin/docker rm ${var.container_name} || true
@@ -79,6 +79,7 @@ resource "null_resource" "docker_build_and_deploy" {
           -v ${var.persistent_data_path}/logs:/app/logs \
           ${var.image_name}
         rm -f shechill-analysis.tar.gz
+        cd ~
         /usr/local/bin/docker ps | grep ${var.container_name}
 ENDSSH
     EOT
